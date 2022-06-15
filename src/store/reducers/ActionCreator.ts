fd0abc54b23048490af3066d27ac709311fdb25a
Yelyzaveta Omelich <omelichelizaveta@gmail.com>
2022-06-15 17:38:52 +0300

    withdraw unnecessary requests

--- src/store/reducers/ActionCreator.ts
+++ src/store/reducers/ActionCreator.ts
@@ -1,27 +1,17 @@
 import axios from "axios";
 import { IData } from "../../models/IColor";
-// import { AppDispatch } from "../store";
-// import { colorSlice } from "./ColorSlice";
 import { createAsyncThunk } from "@reduxjs/toolkit";
+
+const url = process.env.REACT_APP_SERVER_URL as string;
 
 export const fetchColors = createAsyncThunk(
   'color/fetchAll',
   async(_, thunkAPI) => {
     try {
-      const response = await axios.get('https://reqres.in/api/products');
+      const response = await axios.get(url);
       return response.data.data as IData[];
     } catch (error) {
       thunkAPI.rejectWithValue('Failed to load data')
     }
   }
 );
-
-// export const fetchColors = () => async (dispatch: AppDispatch) => {
-//   try {
-//     dispatch(colorSlice.actions.colorsFetching())
-//     const response = await axios.get<IColor[]>('https://reqres.in/api/products')
-//     dispatch(colorSlice.actions.colorsFetchingSuccess(response.data))
-//   } catch (e: any) {
-//     dispatch(colorSlice.actions.colorsFetchingError(e.message))
-//   }
-// }
--- src/store/reducers/ColorSlice.ts
+++ src/store/reducers/ColorSlice.ts
@@ -23,18 +23,6 @@ export const colorSlice = createSlice({
     search: (state, action) => {
       state.searchText = action.payload;
     }
-    // colorsFetching(state) {
-    //   state.isLoading = true;
-    // },
-    // colorsFetchingSuccess(state, action: PayloadAction<IColor[]>) {
-    //   state.isLoading = false;
-    //   state.error= '';
-    //   state.colors = action.payload;
-    // },
-    // colorsFetchingError(state, action: PayloadAction<string>) {
-    //   state.isLoading = false;
-    //   state.error = action.payload;
-    // }
   },
   extraReducers: {
     [fetchColors.fulfilled.type]: (state, action: PayloadAction<IData[]>) => {
