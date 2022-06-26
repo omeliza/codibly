1ad06ebe47dcafb775485b529f18c763f33aad92
Yelyzaveta Omelich <omelichelizaveta@gmail.com>
2022-06-26 11:55:27 +0300

    menu icon removed & added link to deployed project

--- src/components/SearchAppBar/SearchAppBar.tsx
+++ src/components/SearchAppBar/SearchAppBar.tsx
@@ -2,9 +2,7 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
 import { search } from '../../store/slices/colorSlice';
 import { useAppDispatch, useAppSelector } from '../../hooks/hooks';
@@ -17,15 +15,6 @@ export default function SearchAppBar() {
     <Box sx={{ flexGrow: 1, mb: '20px' }}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="open drawer"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             noWrap
